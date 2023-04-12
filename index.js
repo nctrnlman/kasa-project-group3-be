e0e5ad311053b1dd5593cd929e9116f57201db2a
Rhazes Devino <rhazesd@gmail.com>
2023-04-12 19:50:02 +0700

    initialization

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const PORT = 8001;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  console.log("Start...");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port: " + PORT);
+});
