3ca2f8ea5f3ba116a727de4bfa7ba349cd69ec63
Rhazes Devino <rhazesd@gmail.com>
2023-04-13 21:15:25 +0700

    add database

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const PORT = 8001;
 const app = express();
+const cors = require("cors");
+const { db, query } = require("./database");
+
+app.use(cors());
 
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  console.log("Start...");
+// app.get("/", async (req, res) => {
+//   console.log("Start...");
+// });
+app.get("/user", async (req, res) => {
+  let fetchQuery = "SELECT * FROM user";
+  db.query(fetchQuery, (err, result) => {
+    return res.status(200).send(result);
+  });
 });
 
 app.listen(PORT, () => {
