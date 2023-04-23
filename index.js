0470ed2b9d5087897a58f74d597e3004939098ff
Rhazes Devino <rhazesd@gmail.com>
2023-04-23 20:50:32 +0700

    add register & login

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const PORT = 8001;
 const app = express();
 const cors = require("cors");
 const { db, query } = require("./database");
+const { authRoutes } = require("./routes");
 
 app.use(cors());
 
@@ -11,12 +12,14 @@ app.use(express.json());
 // app.get("/", async (req, res) => {
 //   console.log("Start...");
 // });
-app.get("/user", async (req, res) => {
-  let fetchQuery = "SELECT * FROM user";
-  db.query(fetchQuery, (err, result) => {
-    return res.status(200).send(result);
-  });
-});
+// app.get("/user", async (req, res) => {
+//   let fetchQuery = "SELECT * FROM user";
+//   db.query(fetchQuery, (err, result) => {
+//     return res.status(200).send(result);
+//   });
+// });
+
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
